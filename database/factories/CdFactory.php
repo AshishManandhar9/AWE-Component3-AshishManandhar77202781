f49185b6ef0fc82d410f901094219a16db484d2c
AshishManandhar9 <aashishmanandhar99@gmail.com>
2020-12-14 09:43:53 +0545

    Initial Commit-Laravel Initialized

---
+++ database/factories/CdFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Cd;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class CdFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Cd::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
