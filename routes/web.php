2ce4844f2dd5ff39f4ebe9e600127e95d40b574f
AshishManandhar9 <aashishmanandhar99@gmail.com>
2020-12-15 09:33:50 +0545

    Layout updated & index.blade page added

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
+});*/
+
+Route::redirect('/', '/index')->name('index');
+
+Route::get("/index", function () {
+    return view('index');
 });
 
 Auth::routes();
